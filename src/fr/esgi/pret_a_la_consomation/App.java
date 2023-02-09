32f60f8db8a48556b58aa1cecffae67e93a344dd
Matthieu Flce <flacelierematthieu@gmail.com>
2023-02-09 12:00:00 +0100

    feat : ajout des premiers menu

--- src/fr/esgi/pret_a_la_consomation/App.java
+++ src/fr/esgi/pret_a_la_consomation/App.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     private static final TauxService tauxService = new TauxServiceImpl();
@@ -18,10 +19,9 @@ public class App {
     private static final ClientService clientService = new ClientServiceImpl();
     private static final PretService pretService = new PretServiceImpl();
     private static final MensualiteService mensualiteService = new MensualiteServiceImpl();
-
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         motifService.ajouterMotif("Moto", "Moto desc");
         motifService.ajouterMotif("Auto", "Auto desc");
 
@@ -35,6 +35,9 @@ public class App {
         for (int i = 0; i < 5; i++) {
             clientService.ajouterClient("Nom" + i, "Prenom" + i);
         }
+
+        //Aprer initialisation des clients et taux
+        menuPrincipal();
 
         for (Taux taux : tauxService.recupererTauxs()) {
             System.out.println(taux.toStringConsole());
@@ -51,5 +54,90 @@ public class App {
         pretService.trierPret("taux");
         System.out.println(pretService.recupererPrets());
 
+    }
+
+    /*
+    * Menu principal
+    */
+    private static void menuPrincipal(){
+        byte selection = 0;
+        System.out.println("""
+        Bienvenue sur prêt à la consommation\s
+        1. Voir tous les prêts triées par montant (du plus élevé au plus petit)
+        2. Voir tous les prêts triées par taux (du plus élevé au plus petit)\s
+        3. Voir la liste des prêts qui débutent entre deux dates données\s
+        4. Ajouter un prêt
+        5. Quitter \s
+        """);
+        do {
+            try {
+                System.out.print("Faîtes votre choix : ");
+                selection = Byte.parseByte(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Saisie invalide");
+            }
+        } while (selection < 1 || selection > 5);
+        switch (selection) {
+            case 1 -> affichageTriesPrets("montant");
+            case 2 -> affichageTriesPrets("taux");
+            case 3 -> affichageDatesPrets();
+            case 4 -> affichageAjoutPret();
+        }
+    }
+
+    /*
+     * Affichage des prêts avec des tries ou des date données
+     */
+    private static void affichageTriesPrets(String trie){
+        pretService.trierPret(trie);
+        // Améliorer l'affichage
+        System.out.println(pretService.recupererPrets());
+    }
+
+    /*
+     * Affichage pour ajouter un pret
+     */
+    private static void affichageAjoutPret(){
+        affichageClients();
+        System.out.println("Veuillez saisir l'id du client concerné : ");
+        //TODO
+        System.out.println("Veuillez saisir le montant demandé : ");
+        //TODO
+        affichageTaux();
+        System.out.println("Veuillez saisir l'id du taux annuel : ");
+        //TODO
+        System.out.println("Veuillez saisir la date d'effet au format MM/yyyy : ");
+        //TODO
+        Pret newPret = new Pret(1000, LocalDateTime.now(), LocalDate.of(2023, 03, 01), "", tauxService.recupererTaux(1L), clientService.recupererClient(1L));
+        pretService.ajouterPret(newPret);
+        affichagePret(newPret);
+    }
+
+    /*
+    *   Affichage des prets entre deux dates
+    */
+    private static void affichageDatesPrets(){
+
+    }
+
+    /*
+     * Affichage des clients
+     */
+    private static void affichageClients(){
+
+    }
+
+    /*
+     * Affichage des taux
+     */
+    private static void affichageTaux(){
+
+    }
+
+    /*
+    * Affichage en détail d'un prêts
+    */
+    private static void affichagePret(Pret pret){
+        System.out.println(pret);
     }
 }
